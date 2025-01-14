7ba1bebdb93342dffaad4f20d4547f5dafb11051
khushant2001 <khushant.khurana11@gmail.com>
2025-01-14 00:01:36 -0500

    Fixing indexing issue

--- car_HJB.py
+++ car_HJB.py
@@ -5,6 +5,11 @@ Discrete HJB equation:
 u_optimal = argmin{u}[l(x,u) + V*(f(x),n+1)] => recursive over the optimal cost function!
 
 Strategy: TREAT THIS AS A COST FUNCTION!
+
+PROS: Can be done offline and then hope for the best for the hardware to perform like the simulated model!
+CONS: Needs a longer time horizon. Doesn't matter if you increase the terminal cost. The car wouldn't reach the final spot. 
+Also for some cases, it is giving some weird results (BLATANTLY WRONG)! Like you need specific combinations of the horizon time
+and step time to make the HJB converge. Weird!
 """
 
 from casadi import *
@@ -19,8 +24,8 @@ def toy_car_HJB():
     # y_dot = vsin(theta)
     # theta_dot = omega
     
-    time_horizon = 4 # The total time given for the trajectory!
-    step_time = 0.2 #sec when does the observation come!
+    time_horizon = 5 # The total time given for the trajectory!
+    step_time = 0.1 #sec when does the observation come!
     steps = int(time_horizon/step_time)
     v_min = -.6 # Min linear velocity
     v_max = .6 # Max linear velocity
@@ -46,8 +51,10 @@ def toy_car_HJB():
     rhs = vertcat(v*cos(theta),v*sin(theta),w)
     dynamics = Function('dynamics',[state_vector,control_vector],[rhs])
 
-    state_prediction = SX.sym('X',n_states,steps+1) # The recorded states over the trajectory!
-    control_prediction = SX.sym('U',n_control,steps)
+    # Building the state and control prediction matrices! PAY Attention to the size. Since indexing starts at 0, the 
+    # state size is steps but control size is steps-1. This convention is followed later in the code!
+    state_prediction = SX.sym('X',n_states,steps) # The recorded states over the trajectory!
+    control_prediction = SX.sym('U',n_control,steps-1) # The optimized control inputs!
 
     # Define the parameters vector: initial and final point!
     p = SX.sym('p',n_states*2)
@@ -56,7 +63,7 @@ def toy_car_HJB():
     state_prediction[:,0] = p[0:3]
 
     # Populating the state_prediction matrix!
-    for i in range(steps):
+    for i in range(steps-1):
         state_prediction[:,i+1] = state_prediction[:,i] + step_time*dynamics(state_prediction[:,i],control_prediction[:,i])
 
     # Define the objective function: which is basically a quadratic cost function
@@ -75,17 +82,17 @@ def toy_car_HJB():
     T = 100*DM.eye(n_states)
 
     # Actual Terminal cost
-    l_terminal = ((state_prediction[steps] - p[3:6]).T @ T @ (state_prediction[steps] - p[3:6]))
+    l_terminal = ((state_prediction[steps-1] - p[3:6]).T @ T @ (state_prediction[steps-1] - p[3:6]))
 
     # Making a dictionary to store the cost at individual . This will help for recursive cost propagation!
     V = dict()
-    V[steps] = l_terminal # Cost at the terminal step!
+    V[steps-1] = l_terminal # Cost at the terminal step!
 
     # Initiating the cost function!
     obj = l_terminal
 
     # Summing over the steps to build up the cost function!
-    for i in range(steps-1, 0, -1):
+    for i in range(steps-2, -1, -1):
         curr_state = state_prediction[:,i]
         curr_input = control_prediction[:,i]
         cost = ((curr_state - p[3:6]).T @ Q @ (curr_state - p[3:6])) + curr_input.T @ R @ curr_input + V[i+1]
@@ -110,52 +117,60 @@ def toy_car_HJB():
     lbg = -2 # so that the x and y are below -2!
     ubg = 2 # so that the x and y are below 2!
 
-    lbx = [v_min if i % 2 == 0 else omega_min for i in range(n_control * steps)]
-    ubx = [v_max if i % 2 == 0 else omega_max for i in range(n_control * steps)]
+    # Constraints for the optimized variables.
+    lbx = [v_min if i % 2 == 0 else omega_min for i in range(n_control * (steps-1))]
+    ubx = [v_max if i % 2 == 0 else omega_max for i in range(n_control * (steps-1))]
 
     # SET UP THE SIMULATION!!!
     x0 = DM([0, 0, 0])  # Initial state
     xf = DM([1, 1, pi])  # Target state
 
-    u0 = DM([0, 0] * steps).T  # Initial control guess
+    u0 = DM([0, 0] * (steps-1)).T  # Initial control guess
 
     # Update the parameters! 
     p_numeric = vertcat(x0, xf)
 
     # Give an initial estimate for the optimization variables!
-    u_0 = reshape(u0, 2*steps, 1) # initial value of the optimization variables!
+    u_0 = reshape(u0, 2*(steps-1), 1) # initial value of the optimization variables!
 
     # Solve the optimization problem!
     sol = solver(x0=u_0, lbx=lbx, ubx=ubx, ubg=ubg, lbg=lbg, p=p_numeric)
 
     # Extract the optimal output!
-    u = reshape(sol['x'], 2, steps) # Optimal solution
+    u = reshape(sol['x'], 2, steps-1) # Optimal solution
     
     # True state
     true_state = [x0.full()]
 
     # Determine the true_state from the optimized control variables!
-    for i in range(steps):
-        updated_state = true_state[i] + step_time*dynamics(true_state[i],u[i])
+    for i in range(steps-1):
+        updated_state = true_state[i] + step_time*dynamics(true_state[i],u[:,i])
         true_state.append(updated_state.full())
 
+    # Converting the list to an array for assisting in plotting!
     true_state = np.array(true_state)
 
+    # Plotting the state info over x vs y plot. 
     plt.figure()
     plt.scatter(true_state[:,0],true_state[:,1])
     plt.scatter(xf[1].full(),xf[0].full(),label = 'Final_destination')
+    plt.scatter(x0[1].full(),x0[0].full(),label = 'Initial_position')
+    plt.xlabel('X[m]')
+    plt.ylabel("Y[m]")
+    plt.title("Optimized Trajectory")
     plt.legend()
     plt.grid()
 
     # Defining a time array for plotting purposes!
-    step_array = np.linspace(0,time_horizon,steps)
+    step_array = np.linspace(0,time_horizon,steps-1)
     plt.figure()
-    plt.plot(step_array,u.full()[0:,].T,label = 'Linear Velocity [m/sec]')
-    plt.plot(step_array,u.full()[1:,].T,label = 'Angular velocity [rad/sec]')
+    plt.plot(step_array,u.full()[0:,].T, label = 'Linear Velocity [m/sec]')
+    plt.plot(step_array,u.full()[1:,].T, label = 'Angular velocity [rad/sec]')
     plt.legend()
     plt.title("Optimized inputs")
     plt.xlabel("Time [sec]")
     plt.grid()
     plt.show()
+
 # Call the function!
 toy_car_HJB()
